8073e98243849045e197e1fc48bd3bc781455824
Charlotte Harrington <c.harrington.0301@gmail.com>
2023-10-19 13:46:01 -0500

    card title wrapping, next images

--- personal-site/app/components/ExperienceCardNew.tsx
+++ personal-site/app/components/ExperienceCardNew.tsx
@@ -1,31 +1,29 @@
 import { IMAGES_MANIFEST } from "next/dist/shared/lib/constants";
 import { moderatRegular } from "../fonts/fonts";
 import Tag from "./Tag";
+import Image from "next/image";
+
 
 export interface IExperienceCardNewProps {
   title: string;
-  imageSrc: string[];
+  imageSrc: typeof Image
 }
-
 export const ExperienceCardNew = ({
   ...props
 }: IExperienceCardNewProps): JSX.Element => {
   return (
-    <div className="my-2 py-4 rounded-[10px] border-solid border-[#581845] border gap-[10px] flex sm:flex-row flex-col items-center justify-start sm:px-4 w-full">
+    <div className="my-2 py-4 px-2 rounded-[10px] border-solid border-[#581845] border gap-[10px] flex flex-wrap items-center justify-between sm:px-4 w-full">
       <div className="flex flex-wrap m-2">
         <div className="flex flex-row items-center">
-        {props.imageSrc.map((image) => {
-          return (
-            <img key={image}
-              className="hover:scale-[100.1%] ease-in-out overflow-hidden shrink-0 max-h-[400px] rounded flex-initial"
-              src={image}
+            <Image
+              className="hover:scale-[105%] duration-500 ease-in-out md:max-w-[700px] rounded flex-initial"
+              src={props.imageSrc as any}
+              alt="Device mockup"
             />
-          );
-        })}
         </div>
       </div>
       <div
-        className={`text-[#581845] h-min text-3xl text-left justify-left ${moderatRegular.className}`}
+        className={`text-[#581845] flex-initial h-min mx-6 font-bold lg:text-5xl text-left  ${moderatRegular.className}`}
       >
         {props.title}
       </div>
--- personal-site/app/page.tsx
+++ personal-site/app/page.tsx
@@ -1,25 +1,29 @@
+import { moderatRegular } from "./fonts/fonts";
 
-import {
-  moderatRegular,
-} from "./fonts/fonts";
+import React, { useEffect, useState } from "react";
+
+import bitjockey from "../public/images/bitjockey_both.png";
+import coursicle from "../public/images/coursicle_mac.png";
+import kanm from "../public/images/kanm_both.png";
+
 import Image from "next/image";
 import headshot from "public/IMG_7295.jpg";
 import { ExperienceCardNew } from "./components/ExperienceCardNew";
-import Typewriter from 'react-ts-typewriter';
+import Typewriter from "react-ts-typewriter";
 
 const Page = () => {
   const projects = [
     {
       title: "Bitjockey",
-      img: ["images/bitjockey_both.png"],
+      img: bitjockey,
     },
     {
       title: "Coursicle Chat",
-      img: ["images/coursicle_mac.png"],
+      img: coursicle,
     },
     {
       title: "KANM Website",
-      img: ["images/kanm_both.png"],
+      img: kanm,
     },
   ];
 
@@ -43,8 +47,8 @@ const Page = () => {
           })}
         </div>
         <div className="flex flex-wrap md:py-40 md:px-10 lg:px-40 px-4 py-10">
-          <div
-            className={`flex-initial sm:text-6xl text-4xl text-[#581845] w-full ${moderatRegular.className}`}
+          <section
+            className={`flex-initial sm:text-6xl text-4xl text-[#581845] w-full border-b ${moderatRegular.className}`}
           >
             <div className="my-10 flex flex-row items-center">
               <div className="sm:mx-[0px]">
@@ -55,41 +59,46 @@ const Page = () => {
               <span className="flex-initial">i&apos;m a</span>
               <span className="flex-initial">
                 <span className="relative sm:rounded-[10px] rounded-[5px] border-solid border-[#581845] border mx-2 min-[1125px]:mr-6 px-2">
-                <span className="-z-10 invisible lg:mx-6">
-                  {" "}full-stack developer{" "}
-                </span>
-                <span className="absolute left-[4px] top-[-10x] flex-initial items-center z-20 ">
-                  <Typewriter
-                    text={[
-                      " UI/UX designer",
-                      " database architect",
-                      " full-stack developer",
-                    ]}
-                    delay={1500}
-                    speed={50}
-                    random={50}
-                    onFinished={() => {
-                      // setTimeout(() => setIsPopup2Open(true), 1000);
-                    }}
-                  />
-                </span>
+                  <span className="-z-10 invisible lg:mx-6">
+                    {" "}
+                    full-stack developer{" "}
+                  </span>
+                  <span className="absolute left-[4px] top-[-10x] flex-initial items-center z-20 ">
+                    <Typewriter
+                      text={[
+                        " UI/UX designer",
+                        " database architect",
+                        " full-stack developer",
+                      ]}
+                      delay={1500}
+                      speed={50}
+                      random={50}
+                      onFinished={() => {
+                        // setTimeout(() => setIsPopup2Open(true), 1000);
+                      }}
+                    />
+                  </span>
                 </span>
               </span>
-   
+
               <span className="flex-initial transition ease-in-out">
                 focused on building <b>efficient</b>, <b>beautiful</b>, and{" "}
                 <b>useful</b> products for an innovating world
                 <br />
               </span>
-   
             </div>
             <div className={`flex flex-wrap`}>
               {projects.map((p) => {
-                return <ExperienceCardNew key={p.title} imageSrc={p.img} title={p.title} />;
+                return (
+                  <ExperienceCardNew
+                    key={p.title}
+                    imageSrc={p.img as any}
+                    title={p.title}
+                  />
+                );
               })}
             </div>
-         
-          </div>
+          </section>
         </div>
       </div>
     </div>
