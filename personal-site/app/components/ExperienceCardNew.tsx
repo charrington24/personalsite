ccf9cd1fbe3139f250e6ae4e4eed2b83c06fb067
Charlotte Harrington <c.harrington.0301@gmail.com>
2023-10-18 21:23:37 -0500

    added typewriter thing

--- personal-site/app/components/ExperienceCardNew.tsx
+++ personal-site/app/components/ExperienceCardNew.tsx
@@ -1,31 +1,34 @@
+import { IMAGES_MANIFEST } from "next/dist/shared/lib/constants";
 import { moderatRegular } from "../fonts/fonts";
 import Tag from "./Tag";
 
 export interface IExperienceCardNewProps {
-  title: string
-  imageSrc: string
+  title: string;
+  imageSrc: string[];
 }
 
 export const ExperienceCardNew = ({
   ...props
 }: IExperienceCardNewProps): JSX.Element => {
   return (
-    <div className="sm:w-[506px] sm:m-8 m-2 drop-shadow-md rounded-[10px] border-solid border-[#581845] border gap-[10px] items-start justify-start">
-      <div className={`text-[#581845] px-2 pt-3 h-min text-3xl text-left text-terminal-blue block justify-left border-solid border-[#581845] border-b ${moderatRegular.className}`}>
+    <div className="my-2 py-4 rounded-[10px] border-solid border-[#581845] border gap-[10px] flex sm:flex-row flex-col items-center justify-start sm:px-4 w-full">
+      <div className="flex flex-wrap m-2">
+        <div className="flex flex-row items-center">
+        {props.imageSrc.map((image) => {
+          return (
+            <img
+              className="hover:scale-[100.1%] ease-in-out overflow-hidden shrink-0 max-h-[400px] rounded flex-initial"
+              src={image}
+            />
+          );
+        })}
+        </div>
+      </div>
+      <div
+        className={`text-[#581845] h-min text-3xl text-left justify-left ${moderatRegular.className}`}
+      >
         {props.title}
       </div>
-      <div className="flex flex-wrap m-2">
-        <div className={`flex flex-row gap-[5px] mb-2`}>
-          <Tag content={"internship"} />
-          <Tag content={"jQuery"} />
-          <Tag content={"php"} />
-        </div>
-        <img
-          className="hover:scale-[100.1%] ease-in-out w-full rounded border-solid border-[#581845] border flex-initial"
-          src={props.imageSrc}
-        />
-      </div>
     </div>
-
   );
 };
